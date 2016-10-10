47cd3bd14ab9b8efbd68fe3b67f20060dbfee0b4
Victor Sebben <victorhfsebben@gmail.com>
2016-10-10 15:49:29 -0300

    Initial commit
    Set up initial babel project and mocha testing.

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var expect = require("chai").expect;
+var test = require("../build/test");
+
+describe("Test tests", function() {
+  it("tests tests", function() {
+    expect(5).to.equal(5);
+    expect(2).to.equal(2);
+  });
+});
